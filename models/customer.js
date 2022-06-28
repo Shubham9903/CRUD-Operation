c118e79351583d2d98d65347a7e76715a3389a1b
shubham <shubhamrupapara993@gmail.com>
2022-06-28 13:21:14 +0530

    ..

---
+++ models/customer.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const customerSchema = new mongoose.Schema({
+    firstname: {
+        type: String,
+        minlength: 3,
+        maxlength: 50,
+        required: true
+    },
+    lastname: {
+        type: String,
+        minlength: 3,
+        maxlength: 50,
+        required: true
+    }, 
+    phonenumber: {
+        type: String,
+        minlength: 10,
+        required: true
+    },
+    cnic: {
+        type: String,
+        required: true
+    }, 
+    address: {
+        type: String,
+        required: true
+    }
+});
+
+const Customer = mongoose.model('Customer', customerSchema);
+
+const validateCustomer = (customer) => {
+    const schema = {
+        firstname: Joi.string().min(3).max(50).required(),
+        lastname: Joi.string().min(3).max(50).required(),
+        phonenumber: Joi.string().min(10).required(),
+        cnic: Joi.string().required(),
+        address: Joi.string().required()
+    }
+
+    return Joi.validate(customer, schema);
+}
+
+
+module.exports.Customer = Customer;
+module.exports.validate = validateCustomer;
